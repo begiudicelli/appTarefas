6bcb9f257112086ddab8be5b765f69f3496cb364
Bernardo Giudicelli <giudicelli.be@gmail.com>
2024-11-03 02:41:06 -0300

    added feature/about/adicionar/estatisticas components and made routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,19 @@ import { RegistrarComponent } from './componentes/autenticacao/registrar/registr
 import { TarefasComponent } from './componentes/tarefas/tarefas.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { AuthGuard } from './componentes/autenticacao/AuthGuard';
+import { FeaturesComponent } from './componentes/features/features.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { AdicionarComponent } from './componentes/adicionar/adicionar.component';
+import { EstatisticasComponent } from './componentes/estatisticas/estatisticas.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registrar', component: RegistrarComponent },
     { path: 'tarefas', component: TarefasComponent, canActivate:[AuthGuard] },
     { path: 'home', component: HomeComponent},
+    { path: 'features', component: FeaturesComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'adicionar', component: AdicionarComponent},
+    { path: 'estatisticas', component: EstatisticasComponent},
     { path: '', component: HomeComponent}   
 ];
