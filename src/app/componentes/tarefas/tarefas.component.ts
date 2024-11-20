78daeae5af677b57cfd854db2a42bccbfe4d142c
LuisaSantana <lu.isa.santana@hotmail.com>
2024-11-20 18:35:38 -0300

    create list-task menu

--- src/app/componentes/tarefas/tarefas.component.ts
+++ src/app/componentes/tarefas/tarefas.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-tarefas',
   standalone: true,
   imports: [],
   templateUrl: './tarefas.component.html',
-  styleUrl: './tarefas.component.css'
+  styleUrl: './tarefas.component.css',
+ 
 })
 export class TarefasComponent {
 
